6c248763a6785cdea6740903f6d90471c6ff53d0
Mladen Stajčić <diskozub@hotmail.com>
2016-04-08 16:17:59 +0200

    slanje mejla

--- ISA-Tim7/src/services/UserService.java
+++ ISA-Tim7/src/services/UserService.java
@@ -1,6 +1,15 @@
 package services;
 
+import java.util.Properties;
+
 import javax.inject.Singleton;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -36,6 +45,42 @@ public class UserService {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public void addGuest(JSONObject data){
+		final String username = "odjebigaguglu@gmail.com";
+		final String password = "kuracbakin123 ";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+ 			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("nebitno@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse((String)data.get("email")));
+			message.setSubject("Activating account");
+			message.setText("<a href='127.0.0.1:8080/ISA-Tim7/index.html'>activate account</a>", "utf-8", "html");
+		
+			
+			Transport.send(message);
+
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+
+		
+		
+		
 		UserDAO.addGuest((String)data.get("email"), (String)data.get("fname"), (String)data.get("lname"), (String)data.get("pwd"));
 	}
 	
